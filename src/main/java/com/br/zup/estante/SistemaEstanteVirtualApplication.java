adea35590b877d46ffa2247b466b73ae96df6dae
Carol <carolina.oliveira@zup.com.br>
2019-07-09 14:42:40 -0300

    Iniciar projeto

---
+++ src/main/java/com/br/zup/estante/SistemaEstanteVirtualApplication.java
@@ -0,0 +1,13 @@
+package com.br.zup.estante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEstanteVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEstanteVirtualApplication.class, args);
+	}
+
+}
